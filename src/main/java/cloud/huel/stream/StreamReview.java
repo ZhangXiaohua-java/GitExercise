c3cc6c1606f8c0539793ac27f8cb6651bce4e0ba
张晓华 <101411494+JavaGoJs@users.noreply.github.com>
2022-05-12 17:00:47 +0800

    develop分支提交

--- src/main/java/cloud/huel/stream/StreamReview.java
+++ src/main/java/cloud/huel/stream/StreamReview.java
@@ -18,7 +18,10 @@ public class StreamReview {
 		String [] strs = {"ab","ac","ad","ae","ff"};
 		long num = Arrays.stream(strs).filter(i->i.startsWith("a")).distinct().count();
 		System.out.println(num == strs.length-1);
-
+		String[] strings = new String [strs.length];
+		strings = strs;
+		strs = null;
+		Arrays.stream(strings).filter(i->i.startsWith("f")).forEach(s-> System.out.println(s));
 	}
 
 

12b9ed3c0a18ff1bc16f920332b2b939028de0f3
张晓华 <101411494+JavaGoJs@users.noreply.github.com>
2022-05-12 17:10:46 +0800

    develop分支提交StreamReview out1

---
+++ src/main/java/cloud/huel/stream/StreamReview2.java
@@ -0,0 +1,17 @@
+package cloud.huel.stream;
+
+/**
+ * @author 张晓华
+ * @version 1.0
+ */
+public class StreamReview2 {
+
+	public static void main(String[] args) {
+
+	}
+
+	public static void stream(){
+		System.out.println("stream1");
+	}
+
+}

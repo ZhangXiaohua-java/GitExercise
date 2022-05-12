d64321920260dbceb44bc2a66e51c08ce38cb60f
张晓华 <101411494+JavaGoJs@users.noreply.github.com>
2022-05-12 21:15:05 +0800

    更新本地库,push到远程库

--- src/main/java/cloud/huel/stream/StreamReview2.java
+++ src/main/java/cloud/huel/stream/StreamReview2.java
@@ -16,6 +16,7 @@ public class StreamReview2 {
 		System.out.println("stream3");
 		System.out.println("代码合并冲突测试master");
 		System.out.println("develop分支代码合并冲突测试");
+		System.out.println("推送代码到github");
 	}
 
 }
